51fa8ed28d856e94e4d9e49880c61507c8390da9
vanessa082 <wahvanessa22@gmail.com>
2024-07-25 05:54:03 +0100

    created a projectSchema object responsible for data validation and wrote a function using it handled the error

--- utils/projectInputValidator.js
+++ utils/projectInputValidator.js
@@ -3,9 +3,14 @@ import Joi from "joi";
 const projectSchema = {
   name: Joi.string().min(3).max(255).required(),
   description: Joi.string().required(),
-
+  deadline: Joi.date().required(),
+  created_at: Joi.date().default(() => new Date(), 'time of creation'),
+  updated_at: Joi.date().default(() => new Date(), 'time of update')
 }
-
+export default function projectValidator () {
+  const { error } = projectSchema.validate(req.body);
+  if(error) return res.status(400).send(error.details[0].message)
+}
 // CREATE TABLE projects (
 //   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 //   name VARCHAR(255) NOT NULL,
