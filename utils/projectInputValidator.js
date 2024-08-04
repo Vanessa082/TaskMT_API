f276a97a58a69cf5399ed1166ce715c2ddb928a4
vanessa082 <wahvanessa22@gmail.com>
2024-08-04 20:00:14 +0100

    worked on the endpoints of task

--- utils/projectInputValidator.js
+++ utils/projectInputValidator.js
@@ -4,11 +4,13 @@ const projectSchema = Joi.object({
   name: Joi.string().min(3).max(255).required(),
   description: Joi.string().required(),
   deadline: Joi.date().required(),
+  status: Joi.string().default("active"),
 });
 
 export default function projectValidator(req, res, next) {
   const { error, value } = projectSchema.validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+
+  if (error) return res.status(400).json({ message: error.details[0].message });
   req.validatedProject = value;
   next();
 }
